af91b9909ea884c287770d735af7f1a0870bfb73
Rui0823 <zhiweizhu83@gmail.com>
2023-12-23 16:55:38 +0800

    main函数

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# 这是一个示例 Python 脚本。
+import argparse
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+parser = argparse.ArgumentParser(description='配置网络参数')
+parser.add_argument('--patch_size', default=8, type=int, help="将最后网络输出变成多少个patch")
+parser.add_argument('--batch_size', default=8, type=int, help="每次传入网络的batch")
+parser.add_argument('--backbone', default="ResNet50", type=str, help="backbone")
+parser.add_argument('--root', default=r'/data',type=str, help='dataset root')
+parser.add_argument('--lr', default=2e-4,type=int, help='学习率')
